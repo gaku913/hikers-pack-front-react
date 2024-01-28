2b0407ee255c88809c7bcad154a6ead3497628bb
Gaku Ito <gaku.i.913@gmail.com>
2024-01-28 14:22:37 +0000

    Setting Axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { Container, CssBaseline } from "@mui/material"
 // import RouterTop from "./pages/RouterTop"
 import { RouterProvider } from "react-router-dom"
 import routes from "./pages/routes"
+import { useEffect } from "react"
+import axiosSetup from "./lib/axios"
 
 export default function App() {
+
+  // Initialization
+  useEffect(() => {
+    axiosSetup();
+  },[])
 
   return (
     <>
---
+++ src/lib/axios.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export default function axiosSetup(): void {
+  // Base URL
+  const baseUrl = import.meta.env.VITE_API_BASE_URL || "";
+  axios.defaults.baseURL = baseUrl.replace(/\/?$/, "/");
+
+  // Development Only
+  if (import.meta.env.MODE === "development") {
+    // Logging Request
+    axios.interceptors.request.use(request => {
+      const { method, baseURL, url, data } = request;
+
+      console.log("axios -> :", request)
+      console.log(`      # ${method}:${baseURL}${url} ${data}`)
+      return request
+    })
+
+    // Logging Response
+    axios.interceptors.response.use(response => {
+      const { statusText, status, data, request: { responseURL } } = response;
+
+      console.log("axios <- :", response)
+      console.log(`      # ${status}:${statusText} ${responseURL} ${data}`)
+      return response
+    })
+  }
+
+}
