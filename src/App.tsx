8e2b8d64cebc6778df2e1ee425265fd5358956e9
Gaku Ito <gaku.i.913@gmail.com>
2024-01-29 02:24:59 +0000

    Change the mount of Axios setting function. Now axiosSetup() is executed when App is reloaded.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { Container, CssBaseline } from "@mui/material"
-// import RouterTop from "./pages/RouterTop"
 import { RouterProvider } from "react-router-dom"
 import routes from "./pages/routes"
-import { useEffect } from "react"
 import axiosSetup from "./lib/axios"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 export default function App() {
   const client = new QueryClient();
 
-  // Initialization
-  useEffect(() => {
-    axiosSetup();
-  },[])
+  axiosSetup();
 
   return (
     <>
