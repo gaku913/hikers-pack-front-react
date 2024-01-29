633665456e686f40c0c5aeb189c90c0a9b2a7004
Gaku Ito <gaku.i.913@gmail.com>
2024-01-29 00:30:17 +0000

    Update Sandbox. Tried API connection.

--- src/pages/sub/Sandbox.tsx
+++ src/pages/sub/Sandbox.tsx
@@ -1,33 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const fetchData = async () => {
+	const { data } = await axios.get("/hello");
+  return data;
+};
+
+type dataType = {
+  id: number,
+  title: string,
+  contents: string,
+} | null
+
 export default function Sandbox() {
+  const [data, setData] = useState<dataType>(null);
+
+  useEffect(() => {
+    fetchData()
+      .then(result => setData(result))
+      .catch(err => console.log(err));
+  },[]);
+
   return (
     <>
     <h1>Sandbox</h1>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
-    <p>xxxxxxxxxxxxxxxx</p>
+    <p>{JSON.stringify(data)}</p>
     </>
   )
 }
