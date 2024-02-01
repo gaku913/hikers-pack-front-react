ded9d285f584b124defaf81a4c53bc0a9f1d1617
Gaku Ito <gaku.i.913@gmail.com>
2024-02-01 07:08:02 +0000

    Settings of Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }]
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.test.{js,ts,jsx,tsx}"],
+  }
 })
