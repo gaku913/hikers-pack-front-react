2c20851d7aded6e75fd45391a578a4fb1c9df8a4
Gaku Ito <gaku.i.913@gmail.com>
2024-02-03 13:22:30 +0000

    Add contens to PacksIndex page

--- src/pages/packs/PacksIndex.tsx
+++ src/pages/packs/PacksIndex.tsx
@@ -1,7 +1,45 @@
+import { AppBar, Button, List, ListItemButton, ListItemText, Toolbar } from "@mui/material";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 export default function PacksIndex() {
+
   return (
     <>
-    <h1>Index</h1>
+      <List>
+        <ListItemButton divider>
+          <ListItemText primary="南八ヶ岳縦走" secondary="2023年8月14日 重量6.5kg" />
+          <ArrowForwardIosIcon/>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="南八ヶ岳縦走" secondary="2023年8月14日 重量6.5kg" />
+          <ArrowForwardIosIcon/>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="南八ヶ岳縦走" secondary="2023年8月14日 重量6.5kg" />
+          <ArrowForwardIosIcon/>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="南八ヶ岳縦走" secondary="2023年8月14日 重量6.5kg" />
+          <ArrowForwardIosIcon/>
+        </ListItemButton>
+      </List>
+
+      <Toolbar variant="dense"/>
+      <AppBar color="inherit" sx={{ top: "auto", bottom: 0 }}>
+        <Toolbar
+          sx={{
+            displayPrint: "flex",
+            justifyContent: "center"
+          }}
+          >
+          <Button
+            variant="outlined"
+            size="medium"
+          >
+            新しい持ち物リストを追加する
+          </Button>
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
