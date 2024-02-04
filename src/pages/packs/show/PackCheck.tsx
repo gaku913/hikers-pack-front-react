49ac1e0dc5455ea6960c8d4cdb051d3b2ac9735e
Gaku Ito <gaku.i.913@gmail.com>
2024-02-04 13:27:45 +0000

    Write PackCheck page

--- src/pages/packs/show/PackCheck.tsx
+++ src/pages/packs/show/PackCheck.tsx
@@ -1,7 +1,44 @@
+import { Checkbox, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+
 export default function PackCheck() {
+  function createData(
+    name: string,
+    weight: number,
+  ) {
+    return { name, weight };
+  }
+
+  const rows = [
+    createData("バックパック20L", 427),
+    createData("ウィンドブレーカー", 135),
+    createData("レインウェア上", 208),
+    createData("レインウェア下", 222),
+  ];
+
   return (
     <>
-      <h1>Check</h1>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox />
+            </TableCell>
+            <TableCell>名前</TableCell>
+            <TableCell>重量(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, id) => (
+            <TableRow key={id} hover>
+              <TableCell padding="checkbox">
+                <Checkbox />
+              </TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.weight}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   );
 }
