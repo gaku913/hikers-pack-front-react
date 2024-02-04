a09b31e017f0ef63fcefee475c2c7295e6aae3cb
Gaku Ito <gaku.i.913@gmail.com>
2024-02-04 09:37:15 +0000

    Update Tests

---
+++ src/__tests__/components/TabBar.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+describe("TabBar", () => {
+  it.todo("create test", async () => {});
+});
