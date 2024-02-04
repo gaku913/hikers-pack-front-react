3d6aefcb62234985e001817e7e38ed914201bf53
Gaku Ito <gaku.i.913@gmail.com>
2024-02-04 09:34:37 +0000

    Update test

--- src/__tests__/pages/packs/PacksIndex.test.tsx
+++ src/__tests__/pages/packs/PacksIndex.test.tsx
@@ -1,9 +1,12 @@
 import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import PacksIndex from "@/pages/packs/PacksIndex";
+import { BrowserRouter } from "react-router-dom";
 
 describe("PacksIndex", () => {
   it("rendered without error", () => {
-    expect(() => render(<PacksIndex/>)).not.toThrow(Error)
+    expect(() => {
+      render(<PacksIndex/>, {wrapper: BrowserRouter})
+    }).not.toThrow(Error)
   });
 });
