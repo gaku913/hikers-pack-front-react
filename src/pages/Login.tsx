f59861b0db1f1559dcaaeca1a2d817ab9b62a1b3
Gaku Ito <gaku.i.913@gmail.com>
2024-02-05 12:09:08 +0000

    Add form value controle to Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,22 +2,54 @@ import AppFrame from "@/components/AppFrame";
 import AuthForm from "@/components/AuthForm";
 import { default as Link } from "@/components/CustomLink";
 import { TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+type formData = {
+  email: string
+  password: string
+};
+
 
 export default function Login() {
+  const [form, setForm] = useState<formData>({
+    email: "",
+    password: "",
+  });
+  const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.currentTarget.name]: e.currentTarget.value
+    });
+  }
+  const handelSubmit = () => {
+    console.log(form);
+  }
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.currentTarget.select();
+  }
+
   return (
     <AppFrame>
       <h1>ログイン</h1>
       <AuthForm
-        leftButton={{ label: "キャンセル" }}
-        rightButton={{ label: "送信" }}
+        leftButton={{ label: "キャンセル", to: "/" }}
+        rightButton={{ label: "送信", onClick: handelSubmit }}
       >
         <TextField
+          value={form.email}
+          onChange={handleForm}
+          onFocus={handleFocus}
           required
           label="メールアドレス"
+          name="email"
         />
         <TextField
+          value={form.password}
+          onChange={handleForm}
+          onFocus={handleFocus}
           required
           label="パスワード"
+          name="password"
           type="password"
         />
       </AuthForm>
