a5c86c9d328e598101f988cc3ad5977e00557f73
Gaku Ito <gaku.i.913@gmail.com>
2024-02-13 13:41:55 +0000

    Add ListItemButton

---
+++ src/components/common/ListItemButton.tsx
@@ -0,0 +1,34 @@
+import { Link, LinkProps } from "react-router-dom";
+import {
+  ListItemButton as MuiListItemButton,
+  ListItemText,
+} from "@mui/material";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+type ListItemButtonProps = (
+  LinkProps &
+  {
+    primary: string
+    secondary: string
+  }
+);
+
+export default function ListItemButton({
+  primary,
+  secondary,
+  ...rest
+}: ListItemButtonProps) {
+  return (
+    <MuiListItemButton
+      component={Link}
+      divider
+      {...rest}
+    >
+      <ListItemText
+        primary={primary}
+        secondary={secondary}
+      />
+      <ArrowForwardIosIcon/>
+    </MuiListItemButton>
+  )
+}
