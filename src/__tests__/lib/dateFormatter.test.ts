78e399b6df49a701cb8954aeab2de59a4b995c64
Gaku Ito <gaku.i.913@gmail.com>
2024-02-14 02:30:58 +0000

    Update dateFormatter

--- src/__tests__/lib/dateFormatter.test.ts
+++ src/__tests__/lib/dateFormatter.test.ts
@@ -3,7 +3,11 @@ import "@testing-library/jest-dom/vitest";
 import { dateFormatter } from "@/lib/dateFormatter";
 
 describe("dateFormatter", () => {
-  it("format date string data", () => {
+  it("format date to slash style", () => {
     expect(dateFormatter("2000-01-01")).toEqual("2000/01/01");
+  });
+  it("format date to kanji style", () => {
+    expect(dateFormatter("2000-01-01", {
+      format: "kanji"})).toEqual("2000年1月1日");
   });
 });
--- src/lib/dateFormatter.ts
+++ src/lib/dateFormatter.ts
@@ -1,12 +1,37 @@
 /**
  * 日付データにフォーマットを適用する
  * @param dateString 例："2000-01-01"
- * @returns          例："2000/01/01"
+ * @returns          例："2000/01/01", "2000年1月1日"
  */
-export function dateFormatter(dateString: string | undefined) {
+type options = {
+    format?: "kanji" | "slash"
+};
+
+
+export function dateFormatter(
+  dateString: string | undefined,
+  options: options = { format: "slash" },
+) {
   if (!dateString) return undefined
+
+  // 日付文字列をDateオブジェクトに変換
   const date = new Date(dateString);
-  const formattedDate = date.toLocaleDateString(
-    "ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" });
+
+  // 年、月、日を取得
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 月は0から始まるため、1を加える
+  const day = date.getDate();
+
+  let formattedDate = dateString;
+  switch (options?.format) {
+    case "slash":
+      formattedDate = date.toLocaleDateString(
+        "ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" });
+      break;
+
+    case "kanji":
+      formattedDate = `${year}年${month}月${day}日`;
+      break;
+  }
   return formattedDate;
 }
