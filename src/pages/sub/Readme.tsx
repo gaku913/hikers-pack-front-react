2d6bb7039327fb61330f452c945af32ff89e978d
Gaku Ito <gaku.i.913@gmail.com>
2024-02-27 11:22:16 +0000

    Update Readme.tsx. Fetch README.md from GitHub.

--- src/pages/sub/Readme.tsx
+++ src/pages/sub/Readme.tsx
@@ -1,9 +1,30 @@
 import AppFrame from "@/components/frame/AppFrame"
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+
 
 export default function Readme() {
+  const url = "https://raw.githubusercontent.com/gaku913/hikers-pack-root/master/README.md";
+
+  const [markdownContent, setMarkdownContent] = useState("");
+
+  useEffect(() => {
+    const fetchMarkdownContent = async () => {
+      try {
+        const response = await fetch(url);
+        const text = await response.text();
+        setMarkdownContent(text);
+      } catch (error) {
+        console.error("Error fetching markdown content:", error);
+      }
+    };
+
+    fetchMarkdownContent();
+  }, []);
+
   return (
     <AppFrame>
-      <h1>Readme</h1>
+      <Markdown>{markdownContent}</Markdown>
     </AppFrame>
   );
 }
