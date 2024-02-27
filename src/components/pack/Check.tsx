fce557d3d998017ccab1e6c707df4f738d39ec36
Gaku Ito <gaku.i.913@gmail.com>
2024-02-27 10:50:21 +0000

    Update Check.tsx. Add table sort.

--- src/components/pack/Check.tsx
+++ src/components/pack/Check.tsx
@@ -2,6 +2,8 @@ import {
   Checkbox, Table, TableBody, TableCell, TableHead, TableRow
 } from "@mui/material";
 import { PackItemsType } from "@/api/types/packItems"
+import { useTableSort } from "@/hooks/useTableSort";
+import TableCellWithSortLabel from "../common/TableCellWithSortLabel";
 
 type CheckProps = {
   packItems: PackItemsType
@@ -14,6 +16,24 @@ export default function Check({
   onSelectAllClick,
   onClick,
 }: CheckProps) {
+  const {
+    order,
+    sortedArray: sortedPackItems,
+    isOrderBy,
+    handleRequestSort,
+  } = useTableSort<PackItemsType>(packItems, ["item", "name"]);
+
+  const headCells = [
+    {
+      key: ["item", "name"],
+      label: "名前"
+    },
+    {
+      key: ["quantity"],
+      label: "個数"
+    },
+  ];
+
   return (
     <Table stickyHeader>
       <TableHead>
@@ -23,15 +43,21 @@ export default function Check({
               onChange={onSelectAllClick}
             />
           </TableCell>
-          <TableCell>名前</TableCell>
-          <TableCell>個数</TableCell>
+          {headCells.map((headCell, index) => (
+            <TableCellWithSortLabel
+              key={index}
+              label={headCell.label}
+              active={isOrderBy(headCell.key)}
+              order={order}
+              onClick={() => handleRequestSort(headCell.key)}
+            />
+          ))}
         </TableRow>
       </TableHead>
       <TableBody>
-        {packItems.map((packItem, index) => (
+        {sortedPackItems.map((packItem, index) => (
           <TableRow
             key={packItem.id}
-            hover
             onClick={onClick && (() => onClick(index))}
           >
             <TableCell padding="checkbox">
